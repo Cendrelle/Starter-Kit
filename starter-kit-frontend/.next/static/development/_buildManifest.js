15217c9e7852020482bd977c9a02b8fbd1035d36
Cendrelle <cendrellefaizoun@gmail.com>
2026-02-24 13:37:10 +0100

    maj

---
+++ starter-kit-frontend/.next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/candidate/pc-request":["static\u002Fchunks\u002Fpages\u002Fcandidate\u002Fpc-request.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcandidate\u002Fpc-request"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
